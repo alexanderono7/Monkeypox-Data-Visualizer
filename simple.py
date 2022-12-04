882f47e32e9ee00fc1304552bba279c742e65a4d
alexanderono7 <alexanderono7@gmail.com>
2022-12-03 18:59:57 -0800

    Working version of 2D map example w/ PyQT5

---
+++ simple.py
@@ -0,0 +1,30 @@
+import os.path
+import sys
+
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWebEngineCore import QWebEngineUrlRequestInterceptor
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+
+class Interceptor(QWebEngineUrlRequestInterceptor):
+    def interceptRequest(self, info):
+        info.setHttpHeader(b"Accept-Language", b"en-US,en;q=0.9,es;q=0.8,de;q=0.7")
+
+
+def mainPyQt5():
+    CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+    filename = os.path.join(CURRENT_DIR, "index.html")
+    app = QApplication(sys.argv)
+    browser = QWebEngineView()
+
+    interceptor = Interceptor()
+    browser.page().profile().setUrlRequestInterceptor(interceptor)
+
+    browser.load(QUrl.fromLocalFile(filename))
+    browser.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    mainPyQt5()
