45fe986fe34e6f348941b7f4b99f0be30c86bfff
alexanderono7 <alexanderono7@gmail.com>
2022-12-04 11:56:30 -0800

    slider now returns datetime object (needs str)

--- pyqt5.py
+++ pyqt5.py
@@ -1,3 +1,4 @@
+import datetime
 import os.path
 import sys
 
@@ -11,10 +12,9 @@ class Interceptor(QWebEngineUrlRequestInterceptor):
     def interceptRequest(self, info):
         info.setHttpHeader(b"Accept-Language", b"en-US,en;q=0.9,es;q=0.8,de;q=0.7")
     
+#def getDate(x: int):
 
 class Example(QWidget):
-    
-
     def __init__(self):
         super().__init__()
 
@@ -45,14 +45,15 @@ class Example(QWidget):
         button.clicked.connect(lambda: self.on_click(browser))
 
         slider = QSlider(self)
-        slider.setGeometry(QRect(190, 100, 160, 16))
+        slider.setGeometry(QRect(190, 100, 600, 16))
         slider.setOrientation(Qt.Horizontal)
+        slider.setRange(0,212)
         slider.valueChanged.connect(self.slidescale)
 
         self.setLayout(vbox)
 
         self.setGeometry(300, 300, 1000, 1000)
-        self.setWindowTitle('QWebEngineView')
+        self.setWindowTitle('CSE412 - DB Final Project, Group #19')
         self.show()
 
     @pyqtSlot()
@@ -62,7 +63,10 @@ class Example(QWidget):
         browser.page().runJavaScript("updateMap(\'test_transition.csv\')")
 
     def slidescale(self, value):
-        print(value)
+        start_date = '05/01/2022'
+        date_1 = datetime.datetime.strptime(start_date, "%m/%d/%Y")
+        end_date = date_1 + datetime.timedelta(days=value)
+        print(end_date)
 
 
   
