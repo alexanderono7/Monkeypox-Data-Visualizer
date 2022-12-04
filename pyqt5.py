9f76be0237f3d7a97546a4fa0125000ce4e4243c
alexanderono7 <alexanderono7@gmail.com>
2022-12-04 07:25:08 -0800

    HTML browser running inside actual window now

--- pyqt5.py
+++ pyqt5.py
@@ -2,7 +2,7 @@ import os.path
 import sys
 
 from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QApplication, QGridLayout, QMainWindow, QTextEdit, QWidget
+from PyQt5.QtWidgets import QApplication, QGridLayout, QMainWindow, QTextEdit, QWidget, QVBoxLayout
 from PyQt5.QtWebEngineCore import QWebEngineUrlRequestInterceptor
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
@@ -10,24 +10,52 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView
 class Interceptor(QWebEngineUrlRequestInterceptor):
     def interceptRequest(self, info):
         info.setHttpHeader(b"Accept-Language", b"en-US,en;q=0.9,es;q=0.8,de;q=0.7")
-
-
-def mainPyQt5():
-    CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-    filename = os.path.join(CURRENT_DIR, "index.html")
-    app = QApplication(sys.argv)
-    browser = QWebEngineView()
-
-    interceptor = Interceptor()
-    browser.page().profile().setUrlRequestInterceptor(interceptor)
-
-    browser.load(QUrl.fromLocalFile(filename))
-    browser.show()
-    sys.exit(app.exec_())
     
-    @pyqtSlot()
-    def on_click(self):
-        print('PyQt5 button click')
 
-if __name__ == "__main__":
-    mainPyQt5()
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        vbox = QVBoxLayout(self)
+
+        # HTML STUFF STARTS HERE
+        CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+        filename = os.path.join(CURRENT_DIR, "index.html")
+        #app = QApplication(sys.argv)
+        browser = QWebEngineView()
+
+        interceptor = Interceptor()
+        browser.page().profile().setUrlRequestInterceptor(interceptor)
+
+        browser.load(QUrl.fromLocalFile(filename))
+
+        vbox.addWidget(browser)
+        # HTML STUFF ENDS HERE
+
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('QWebEngineView')
+        self.show()
+
+    def loadPage(self):
+
+        with open('test.html', 'r') as f:
+
+            html = f.read()
+            self.webEngineView.setHtml(html)
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
